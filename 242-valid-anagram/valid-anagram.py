04d099c302c2c7080def783ab4abce031940db6b
Gagan Tyagi <106432362+Bethegnt@users.noreply.github.com>
2025-10-04 23:15:52 +0530

    Time: 3 ms (98.52%) | Memory: 18.1 MB (33.53%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,13 +1,5 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:  
-        if len(s) != len(t):
-            return False
-        counter = {}
-        for char in s:
-            counter[char] = counter.get(char, 0) + 1
-        for char in t:
-            if char not in counter or counter[char] == 0:
-                return False
-            counter[char] -= 1
+from collections import Counter
 
-        return True    
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
