ebcc9c5442fc0d0942f66a23e206cde47868ee17
Gagan Tyagi <106432362+Bethegnt@users.noreply.github.com>
2026-04-06 15:24:14 +0530

    Time: 16 ms (30.47%) | Memory: 22 MB (83.76%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram_map = defaultdict(list)
-        for s in strs:
-            char_counts = Counter(s).items()
-            immutable_key = frozenset(char_counts)
-            anagram_map[immutable_key].append(s)
-        return list(anagram_map.values())
+        anagramMap = defaultdict(list)
+        for letter in strs:
+            sortedWord = ''.join(sorted(letter))
+            anagramMap[sortedWord].append(letter)
+        return list(anagramMap.values())    
+        
